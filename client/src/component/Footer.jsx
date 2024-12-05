44cd923726f408d5ef69ae6fe14bfae6f14f1cf9
Timqtzy <timothytenido@gmail.com>
2024-12-05 16:34:31 +0800

    update footer

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -13,19 +13,13 @@ function Footer() {
               </h2>
               <nav className="list-none mb-10">
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">
-                    Home
-                  </a>
+                  <a className="text-gray-600 hover:text-gray-800">Home</a>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">
-                    About
-                  </a>
+                  <a className="text-gray-600 hover:text-gray-800">About</a>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">
-                    Blog
-                  </a>
+                  <a className="text-gray-600 hover:text-gray-800">Blog</a>
                 </li>
                 <li>
                   <a className="text-gray-600 hover:text-gray-800">
@@ -33,9 +27,7 @@ function Footer() {
                   </a>
                 </li>
                 <li>
-                  <a className="text-gray-600 hover:text-gray-800">
-                    Contact
-                  </a>
+                  <a className="text-gray-600 hover:text-gray-800">Contact</a>
                 </li>
               </nav>
             </div>
@@ -45,11 +37,13 @@ function Footer() {
               </h2>
               <nav className="list-none mb-10">
                 <li>
-                <a href="https://www.facebook.com/SchoolPressAdvisersMovementInc" className="text-gray-600 hover:text-gray-800">
+                  <a
+                    href="https://www.facebook.com/SchoolPressAdvisersMovementInc"
+                    className="text-gray-600 hover:text-gray-800"
+                  >
                     Facebook
                   </a>
                 </li>
-
               </nav>
             </div>
             <div className="lg:w-1/5 md:w-1/2 w-full px-4">
@@ -69,7 +63,7 @@ function Footer() {
                 SUBSCRIBE
               </h2>
               <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-                <div className="relative w-80 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                <div className="relative w-80 my-2 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
                   <label
                     htmlFor="footer-field"
                     className="leading-7 text-sm text-gray-600"
@@ -83,7 +77,7 @@ function Footer() {
                     className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-[#ffacac] focus:border-customRed text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
-                <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-customRed border-0 py-2 px-6 focus:outline-none hover:bg-customRedHover rounded text-lg transform transition-all duration-[300ms] ease-in delay-[100ms]">
+                <button className="my-2 lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-customRed border-0 py-2 px-6 focus:outline-none hover:bg-customRedHover rounded text-lg transform transition-all duration-[300ms] ease-in delay-[100ms]">
                   Submit
                 </button>
               </div>
@@ -97,53 +91,83 @@ function Footer() {
         </div>
         <div className="bg-gray-800">
           <div className="container px-5 py-6 max-w-screen-xl mx-auto flex items-center sm:flex-row flex-col">
-            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"> <img
-                src={logo} alt="Spam Logo" className="w-10 h-10 text-white rounded-full"/>
-              <span className="ml-3 text-xl text-customRed">SPAM</span> </a>
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              {" "}
+              <img
+                src={logo}
+                alt="Spam Logo"
+                className="w-10 h-10 text-white rounded-full"
+              />
+              <span className="ml-3 text-xl text-customRed">SPAM</span>{" "}
+            </a>
             <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
               © 2024 SPAM —
               <a
-                  href="https://www.facebook.com/TimCanMimic"
-                  rel="noopener noreferrer"
-                  className="text-gray-600 ml-1"
-                  target="_blank"
+                href="https://www.facebook.com/TimCanMimic"
+                rel="noopener noreferrer"
+                className="text-gray-600 ml-1"
+                target="_blank"
               >
                 @Tim
               </a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-              <a href="https://www.facebook.com/SchoolPressAdvisersMovementInc" className="text-gray-500"
-                 target="_blank">
+              <a
+                href="https://www.facebook.com/SchoolPressAdvisersMovementInc"
+                className="text-gray-500"
+                target="_blank"
+              >
                 <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
+                  fill="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
                 >
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </a>
               <a className="ml-3 text-gray-500">
-                <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5"
-                     viewBox="0 0 24 24">
-                  <path
-                      d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                <svg
+                  fill="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
               <a className="ml-3 text-gray-500">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                     className="w-5 h-5" viewBox="0 0 24 24">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </a>
               <a className="ml-3 text-gray-500">
-                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0"
-                     className="w-5 h-5" viewBox="0 0 24 24">
-                  <path stroke="none"
-                        d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="0"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
               </a>

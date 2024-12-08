d926e706400bf267218c16ec5b66d74ec40053e0
Timqtzy <timothytenido@gmail.com>
2024-12-08 13:25:47 +0800

    small changes

--- client/src/component/TeamSection.jsx
+++ client/src/component/TeamSection.jsx
@@ -1,36 +1,41 @@
 import React from "react";
-import logo from "../assets/SpamLogo.png";
+import member1 from "../src/TeamImage/member-1.png";
+import member2 from "../src/TeamImage/member-2.png";
+import member3 from "../src/TeamImage/member-3.png";
+import member4 from "../src/TeamImage/member-4.png";
+import member5 from "../src/TeamImage/member-5.png";
+import member6 from "../src/TeamImage/member-6.png";
 
 const teamMembers = [
   {
     name: "Melanie V. Briones",
     role: "Chief Operating Officer Bot Chairperson",
-    image: "../src/TeamImage/member-1.png",
+    image: member1,
   },
   {
     name: "Richard N. Briones",
     role: "President",
-    image: "../src/TeamImage/member-2.png",
+    image: member2,
   },
   {
     name: "Jonathan V. Sta Ana",
     role: "Corporate Secretary",
-    image: "../src/TeamImage/member-3.png",
+    image: member3,
   },
   {
     name: "Alice R. Ramos",
     role: "Executive Secretary",
-    image: "../src/TeamImage/member-4.png",
+    image: member4,
   },
   {
     name: "Lydia S. Villanueva",
     role: "Vice-President Internal",
-    image: "../src/TeamImage/member-5.png",
+    image: member5,
   },
   {
     name: "June B. Mijares",
     role: "VP for Advocacy Affairs",
-    image: "../src/TeamImage/member-6.png",
+    image: member6,
   },
 ];
 
@@ -41,22 +46,22 @@ const TeamSection = () => {
         <div className="text-left mb-12">
           <h2 className="text-4xl font-bold mb-4">Our people make us great</h2>
           <p className="text-lg mb-2">
-            Here we focus on markets where technology, innovation, can unlock
+            Here we focus on markets where technology and innovation can unlock
             long-term value.
           </p>
           <p className="text-lg mb-6">
-            You'll interact with talented professionals, will be challenged to
-            solve difficult problems and think in new and creative ways.
+            You'll interact with talented professionals, be challenged to solve
+            difficult problems, and think in new and creative ways.
           </p>
           <button className="inline-flex text-white font-semibold bg-customRed border-0 py-2 px-6 focus:outline-none hover:bg-customRedHover rounded text-lg transform transition-all duration-[300ms] ease-in delay-[100ms]">
             View more
           </button>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {teamMembers.map((member, index) => (
             <div
               key={index}
-              className="bg-gray-100 p-6 rounded-lg text-center shadow-sm transition-all duration-300 hover:shadow-[0_0_15px_rgba(220,38,38,0.3)] hover:-translate-y-1  "
+              className="bg-gray-100 p-6 rounded-lg text-center shadow-sm transition-all duration-300 hover:shadow-[0_0_15px_rgba(220,38,38,0.3)] hover:-translate-y-1"
             >
               <img
                 src={member.image}

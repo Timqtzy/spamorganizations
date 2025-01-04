75466acfcc6161d47bd82f165f31e98ba2c5c0e7
Timqtzy <timothytenido@gmail.com>
2025-01-05 01:33:31 +0800

    Fixing Client

--- client/src/pages/BlogPost.jsx
+++ client/src/pages/BlogPost.jsx
@@ -5,10 +5,11 @@ import axios from "axios";
 const BlogPost = () => {
   const { slug } = useParams();
   const [blog, setBlog] = useState(null);
+  const apiUrl = import.meta.env.API_URL || "http://localhost:5000"; // Replace with the correct URL
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/blogs/${slug}`)
+      .get(`${apiUrl}/api/blogs/${slug}`)
       .then((response) => setBlog(response.data))
       .catch((error) => console.error("Blog not found:", error));
   }, [slug]);
